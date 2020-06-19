8bc7ce0b5497c3551cbd20fbacba6c87b15b9e20
Young-devps <jordiibayo@gmail.com>
2020-06-19 12:05:24 +0100

    settings

--- resume/resume/settings.py
+++ resume/resume/settings.py
@@ -134,12 +134,13 @@ STATIC_URL = '/static/'
 
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'media ')
+STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 if os.environ.get('ENV') == 'PRODUCTION':
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
     STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static'),
         os.path.join(BASE_DIR, 'static_in_project')
     ]
 
